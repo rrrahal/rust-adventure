80e86000b329b157ee9873240a072c486723e51b
Rafael Rahal <rrrahal@gmail.com>
2021-09-12 19:20:17 +0100

    finish third lesson

--- weather-cli/src/main.rs
+++ weather-cli/src/main.rs
@@ -1,11 +1,24 @@
+use reqwest::header::USER_AGENT;
+
 fn main() {
     let api_token = std::env::var("API_TOKEN")
         .expect("to be a token");
-    dbg!(api_token);
 
     let mut arg_iterator = std::env::args();
     arg_iterator.next();
     let args: String = arg_iterator.collect();
 
-    dbg!(args);
+    let client = reqwest::blocking::Client::new();
+
+    let response = client
+        .get("https://api.waqi.info/search/")
+        .query(&[("token", api_token), ("keyword", args)])
+        .header(USER_AGENT, "rafael")
+        .send()
+        .expect("a successful request")
+        .json::<serde_json::Value>()
+        .expect("the body to be json");
+
+    dbg!(response);
+
 }
